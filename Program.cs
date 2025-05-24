c907405c908b316a738845478fb2ee8c030f81ff
Adrian <alextravc.342@gmail.com>
2025-05-24 04:26:35 +0000

    add: jwt + sqlserver db

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddSwaggerGen();
 // Configurar DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 
 
